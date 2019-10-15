be54e584ce55aa0d1d3a0c5321dcf5b294f4e962
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 14:12:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code here!
 document.getElementById("main").remove();
-let element = document.head.createElement('h1')
+let element = document.head.querySelector("title").createElement('h1')
+element.
